7f9c95a8fe131570e265c20c27b70d96e1e61da5
Diana Mora <morra.dianna@gmail.com>
2021-02-04 11:19:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+dodger = document.getElementById('dodger'); //selecting the dodger by it's ID in the html
+
+// move dodger to the left, and make sure it doesn't go over the edge
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+  
+//in the above step, we set the rules: we're moving the dodger one pixel to the left 
+//every time the left arrow is pressed on the keyboard. the bottom step 
+//essentially makes the move happen (event)
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+
+// move dodger to the right, and make sure it doesn't go over the edge
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+        if (e.key === "ArrowRight") {
+          moveDodgerRight()
+        }
+})
